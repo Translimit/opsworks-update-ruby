ec44576aa02b75894c2859e2a8f5d141553e5239
hiroki.takaba@translimit.co.jp <hiroki.takaba@translimit.co.jp>
2022-07-19 23:11:41 +0900

    依存ライブラリのパスを変更

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@ package "readline-devel" do
 end
 
 git "/usr/local/rbenv" do
-  repository "git://github.com/sstephenson/rbenv.git"
+  repository "git://github.com/rbenv/rbenv.git"
   reference "master"
   action :sync
 end
@@ -19,7 +19,7 @@ end
 end
 
 git "/usr/local/ruby-build" do
-  repository "git://github.com/sstephenson/ruby-build.git"
+  repository "git://github.com/rbenv/ruby-build.git"
   reference "master"
   action :sync
 end
